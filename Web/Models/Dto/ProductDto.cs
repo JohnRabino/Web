440614b40ccbaa7251b35185c2fc041fc0529d1b
COLLEGELAB-08 <COLLEGELAB-08@COLLEGELAB-08>
2026-09-12 17:17:04 +0800

    Add project files.

---
+++ Web/Models/Dto/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Models.Dto
+{
+    public class ProductDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+    }
+}
